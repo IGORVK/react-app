06bc9bdc406047291b92217db3fe123bd5bbb6ae
Igor Khaletsky <igor.haletskiy@gmail.com>
2017-10-31 14:17:26 +0200

    Celsius to Fahrenheit Conversion System

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function ToonList (props) {
-	const list = props.cartoon
-	return <ul>{list.map((list,index)=><li key={index}>{list}</li>)}</ul>
+class Calculator extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {scale:'c',temp:0}
+	}
+handleCelcius = (e) => {
+		this.setState({scale:'c', temp:e.target.value})
+		console.log('msg')
 }
-const cartoons = ['Pikachu', 'Alladin', 'Tom', 'Tom'];
+handleFahrenheit = (e) => {
+			this.setState({scale:'f', temp:e.target.value})
+}
 
-
+	render() {
+		const temp = this.state.temp
+		const scale = this.state.scale
+		const celsius = scale==='f'? (temp-32)*5/9 : temp
+		const fahrenheit = scale==='c'? (temp*9/5) + 32 : temp
+		return(
+				<div>
+					<fieldset>
+					<legend>Scale Celsius</legend>
+					<input value={celsius} onChange = {this.handleCelcius}/>
+					</fieldset>
+					<fieldset>
+					<legend>Scale Fahrenheit</legend>
+					<input value={fahrenheit} onChange = {this.handleFahrenheit}/>
+					</fieldset>
+				</div>
+			)
+	}
+}
 
 ReactDOM.render(
-<ToonList cartoon = {cartoons}/>,
+<Calculator />,
 document.getElementById('root')
-	)
+	);
